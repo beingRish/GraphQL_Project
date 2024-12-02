63d5c00ea63ddcbc96d30e035971d402af4ea40c
beingRish <rishabhjuly11@gmail.com>
2024-12-02 12:00:03 +0530

    Mutation, Input Type and Aliases

---
+++ resolvers.js
@@ -0,0 +1,26 @@
+import { users, quotes } from './fakedb.js';
+import { randomBytes } from 'crypto';
+
+const resolvers = {
+    Query: {
+        users: () => users,
+        user: (_, { id }) => users.find(user => user.id == id),
+        quotes: () => quotes,
+        iquote: (_, { by }) => quotes.filter(quote => quote.by == by) 
+    },
+    User: {
+        quotes: (user) => quotes.filter(quote => quote.by == user.id)
+    },
+    Mutation: {
+        signupUserDummy: (_, { userNew }) => {
+            const id = randomBytes(5).toString("hex");
+            users.push({
+                id,
+                ...userNew
+            })
+            return users.find(user => user.id == id);
+        }
+    }
+}
+
+export default resolvers
--- server.js
+++ server.js
@@ -1,48 +1,8 @@
-import {
-    ApolloServer,
-    gql
-} from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import typeDefs from './schemaGQL.js';
+import resolvers from './resolvers.js';
 
-import {
-    ApolloServerPluginLandingPageGraphQLPlayground
-} from 'apollo-server-core';
-
-import { users, quotes } from './fakedb.js';
-
-const typeDefs = gql`
-    type Query {
-        users: [User]
-        user(id: ID!): User
-        quotes: [Quote]
-        iquote(by: ID!): [Quote]
-    }
-
-    type User {
-        id: ID!
-        firstName: String
-        lastName: String
-        email: String
-        password: String
-        quotes: [Quote]
-    }
-
-    type Quote {
-        name: String
-        by: ID
-    }
-`
-
-const resolvers = {
-    Query: {
-        users: () => users,
-        user: (_, { id }) => users.find(user => user.id == id),
-        quotes: () => quotes,
-        iquote: (_, { by }) => quotes.filter(quote => quote.by == by) 
-    },
-    User: {
-        quotes: (user) => quotes.filter(quote => quote.by == user.id)
-    }
-}
 
 const server = new ApolloServer({
     typeDefs,
