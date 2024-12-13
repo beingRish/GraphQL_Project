219090b9e20697609a8918bff6afdb21ff716e14
beingRish <rishabhjuly11@gmail.com>
2024-12-13 17:08:01 +0530

    Authorization and Create Quote.

--- resolvers.js
+++ resolvers.js
@@ -5,6 +5,7 @@ import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from './config.js';
 
 const User = mongoose.model("User");
+const Quote = mongoose.model("Quote");
 
 const resolvers = {
     Query: {
@@ -19,7 +20,6 @@ const resolvers = {
     Mutation: {
         signupUser: async (_, { userNew }) => {
             const user = await User.findOne({email: userNew.email})
-            console.log(user)
             if(user) {
                 throw new Error("User already exists with that email")
             }
@@ -36,17 +36,26 @@ const resolvers = {
 
         signinUser: async (_, { userSignin }) => {
             const user = await User.findOne({email: userSignin.email})   
-            console.log(user)
             if(!user) {
-                throw new Error("User doesn't exists with that email")
+                throw new Error("User doesn't exists with that email.")
             }
             const doMatch = await bcrypt.compare(userSignin.password, user.password)
             if(!doMatch) {
-                throw new Error("email or password is invalid");
+                throw new Error("email or password is invalid.");
             }
             const token = jwt.sign({userId: user._id}, JWT_SECRET)
             return {token}
         },
+
+        createQuote: async (_, { name }, { userId }) => {
+            if(!userId) throw new Error("You must be logged in!")
+            const newQuote = new Quote({
+                name,
+                by: userId
+            })
+            await newQuote.save()
+            return "Quote saved successfully!"
+        }
     }
 }
 
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import { ApolloServer } from 'apollo-server';
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import typeDefs from './schemaGQL.js';
+import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
-import { MONGO_URI } from './config.js';
+import { JWT_SECRET, MONGO_URI } from './config.js';
 
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
@@ -26,6 +27,13 @@ import resolvers from './resolvers.js';
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context: ({ req }) => {
+        const { authorization } = req.headers
+        if(authorization) {
+            const { userId } = jwt.verify(authorization, JWT_SECRET)
+            return { userId }
+        }
+    },
     plugins: [
         ApolloServerPluginLandingPageGraphQLPlayground
     ]
