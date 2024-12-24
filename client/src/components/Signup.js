388b7ba5d0727eaa58891e7f8e50c30f1e9fcc91
beingRish <rishabhjuly11@gmail.com>
2024-12-24 17:23:34 +0530

    useMutation and Signup user

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useMutation } from '@apollo/client'
+import { SIGNUP_USER } from '../gqlOperations/mutations'
 
 export default function Signup() {
   const [formData, setFormData] = useState({})
+  const [signupUser, {data, loading, error}] = useMutation(SIGNUP_USER)
+  if(loading) return <h1>loading</h1>
 
   const handleChange = (e) => {
     setFormData({
@@ -13,10 +17,24 @@ export default function Signup() {
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    signupUser({
+      variables: {
+        userNew: formData
+      }
+    })
   }
 
   return (
     <div className='container my-container'>
+      {
+        error &&
+        <div className='red card-panel'>{error.message}</div>
+      }
+
+      {
+        data && data.user &&
+        <div className='green card-panel'>{data.user.firstName} is signedUp. You can login now!</div>
+      }
       <h5>Signup!!</h5>
       <form onSubmit={(e) => handleSubmit(e)}>
         <input
